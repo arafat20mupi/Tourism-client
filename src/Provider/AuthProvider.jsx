91b0acfa2ed69ba08879c4f6b686485b42adad2d
Arafat islam <arafat.innotimes.net@gmail.com>
2024-04-25 20:11:04 +0600

    first commit

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,80 @@
+
+import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, GithubAuthProvider, onAuthStateChanged, signInWithPopup, signOut, signInWithEmailAndPassword, updateProfile, TwitterAuthProvider } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+
+const auth = getAuth(app);
+export const AuthContext = createContext(null)
+const AuthProvider = ({ children }) => {
+
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
+
+    const signInUser = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+
+    }
+
+    const signOutUser = () => {
+        setLoading(true)
+        signOut(auth)
+    }
+
+    const GoogleProvider = new GoogleAuthProvider();
+    const createGoogleUser = () => {
+        setLoading(true)
+        return signInWithPopup(auth, GoogleProvider)
+    }
+
+    const githubProvider = new GithubAuthProvider();
+    const createGithubUser = () => {
+        setLoading(true)
+        return signInWithPopup(auth, githubProvider)
+    }
+    const twitterProvider = new TwitterAuthProvider();
+    const createTwitterUser = () => {
+        setLoading(true)
+        return signInWithPopup(auth, twitterProvider)
+    }
+
+    const updateUser = (userData) => {
+        setLoading(false);
+        return updateProfile(auth.currentUser, userData);
+    };
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, user => {
+            setUser(user)
+            setLoading(false)
+        });
+        return () => {
+            unSubscribe();
+        }
+    }, [])
+
+
+
+    const authInfo = {
+        createUser,
+        setUser,
+        user,
+        signOutUser,
+        signInUser,
+        loading,
+        createGithubUser,
+        createGoogleUser,
+        createTwitterUser,
+        updateUser,
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
