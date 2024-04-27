28e628ee4095c6e0c285f5f597a2e9aa124b3ccf
Arafat islam <arafat.innotimes.net@gmail.com>
2024-04-27 23:10:58 +0600

    Error Page & extra Page Added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,29 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "../Components/Banner";
 import TouristsCard from "../Components/TouristsCard";
+import OurTeam from "../Components/OurTeam";
+import FeadBack from "../Components/FeadBack";
 
 const Home = () => {
     const tourists = useLoaderData();
     return (
-        <div className="space-y-4">
-            <Banner></Banner>
-
-            <div className="flex flex-col justify-center items-center ">
+        <div className="">
+            <div>
+                <Banner></Banner>
+            </div>
+            <div className="flex flex-col mt-6 lg:mt-24 justify-center items-center ">
                 <h2 className="text-3xl  font-bold tracking-wide"> All Tourist Spots</h2>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 mt-3 lg:mt-12 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {
                     tourists.slice(0, 6).map(tourist => <TouristsCard key={tourist._id} tourist={tourist}></TouristsCard>)
                 }
+            </div>
+            <div className="mt-6 lg:mt-24">
+                <OurTeam></OurTeam>
+            </div>
+            <div className="mt-6 lg:mt-24">
+                <FeadBack></FeadBack>
             </div>
         </div>
     );
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -9,45 +9,55 @@ import MyList from "../Pages/MyList";
 import TouristsCardDetails from "../Components/TouristsCardDetails";
 import PrivateRouter from "./PrivateRouter";
 import Update from "../Components/Update";
+import EroorPage from "../Pages/EroorPage";
 const router = createBrowserRouter([
     {
         path: '/',
+        errorElement: <EroorPage />,
         element: <Root></Root>,
         children: [
             {
                 path: '/',
+                errorElement: <EroorPage />,
                 element: <Home></Home>,
                 loader: () => fetch('https://y-ochre-six.vercel.app/tourists'),
             },
             {
                 path: '/tourists/:id',
+                errorElement: <EroorPage />,
                 element: <PrivateRouter><TouristsCardDetails></TouristsCardDetails> </PrivateRouter>,
                 loader: ({ params }) => fetch(`https://y-ochre-six.vercel.app/tourists/${params.id}`),
             },
             {
                 path: "/login",
+                errorElement: <EroorPage />,
                 element: <Login></Login>
             },
             {
                 path: "/signup",
+                errorElement: <EroorPage />,
                 element: <Signup></Signup>
             },
             {
                 path: '/addSpot',
+                errorElement: <EroorPage />,
                 element: <AddSpot></AddSpot>
             },
             {
                 path: '/allSpot',
+                errorElement: <EroorPage />,
                 element: <AllSpot></AllSpot>,
                 loader: () => fetch('https://y-ochre-six.vercel.app/tourists'),
             },
             {
                 path: "/update/:id",
+                errorElement: <EroorPage />,
                 element: <Update></Update>,
                 loader: ({ params }) => fetch(`https://y-ochre-six.vercel.app/user/${params.id}`),
             },
             {
                 path: '/myList',
+                errorElement: <EroorPage />,
                 element: <PrivateRouter><MyList></MyList></PrivateRouter>,
                 loader: () => fetch('https://y-ochre-six.vercel.app/user'),
             },
